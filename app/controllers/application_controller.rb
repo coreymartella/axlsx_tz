9bd9f7f17e6c22d7a995d9a35cfe20e1c04e2dbf
Corey Martella <cmartella@miovision.com>
2016-05-03 15:48:09 -0400

    rails_apps_composer: add Bootstrap page layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  around_filter :set_time_zone
+
+  def tz
+    @time = Time.zone.now
+  end
+  protected
+    def set_time_zone
+      Time.use_zone("America/Toronto"){yield}
+    end
 end
